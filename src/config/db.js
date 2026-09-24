83ef70905b8e55bd7ef2dcf7c197d654dd715520
laisavargas-blip <lais_a_vargas@estudante.sesisenai.org.br>
2026-09-24 15:53:09 -0300

    começo 3

--- src/config/db.js
+++ src/config/db.js
@@ -6,11 +6,11 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGNAME
 });
 
 export default pool;
--- src/services/produto.service.js
+++ src/services/produto.service.js
@@ -2,13 +2,29 @@ import pool from "../config/db.js";
 
 class ProdutoService {
 
+    // Buscar todos os produtos
     async getAll() {
-        const res = await pool.query("SELECT * FROM produtos");
+
+        // Verificar qual banco a API está utilizando
+        const teste = await pool.query(
+            "SELECT current_database() AS banco, current_schema() AS schema"
+        );
+
+        console.log("Banco conectado pela API:", teste.rows[0]);
+
+        // Buscar os produtos
+        const res = await pool.query(
+            "SELECT * FROM public.produtos"
+        );
+
         return res.rows;
     }
+
+    // Criar um produto
     async create(dados) {
+
         const res = await pool.query(
-            `INSERT INTO produtos 
+            `INSERT INTO produtos
             (nome, marca, categoria, preco, quantidade_estoque)
             VALUES ($1, $2, $3, $4, $5)
             RETURNING *`,
@@ -20,7 +36,10 @@ class ProdutoService {
                 dados.quantidade_estoque
             ]
         );
+
         return res.rows[0];
     }
+
 }
+
 export default ProdutoService;
